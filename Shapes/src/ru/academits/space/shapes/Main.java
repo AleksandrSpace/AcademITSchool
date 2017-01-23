ceab1d26951487950d70e260d48f3a5c4a3b13d8
Александр <aleksandrpustota@gmail.com>
2017-01-23 21:55:50 +0700

    Первый коммит

---
+++ Shapes/src/ru/academits/space/shapes/Main.java
@@ -0,0 +1,10 @@
+package ru.academits.space.shapes;
+
+/**
+ * Created by Александр on 23.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
