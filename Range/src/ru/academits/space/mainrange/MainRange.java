956f18081e2c857a74b4dabf4a65dc8554fe051f
Александр <aleksandrpustota@gmail.com>
2017-02-05 20:42:25 +0700

    Задача Range*

--- Range/src/ru/academits/space/mainrange/MainRange.java
+++ Range/src/ru/academits/space/mainrange/MainRange.java
@@ -21,27 +21,38 @@ public class MainRange {
         double interval = range.calcLength();
         System.out.printf("Длина первого интервала: %.3f%n", interval);
 
-        System.out.println("Введите начало и конец второго диапазона: ");
+        System.out.println("Введите начало и конец второго интервала: ");
 
         from = in.nextDouble();
         to = in.nextDouble();
 
         Range range2 = new Range(from, to);
 
-        range2.setFrom(from);
-        range2.setTo(to);
-
         double interval2 = range2.calcLength();
         System.out.printf("Длина второго интервала: %.3f%n", interval2);
 
-// Range getIntersection(range2)
+        Range range3 = range.getIntersection(range2);
+        if (range3 == null) {
+            System.out.println("Пересечений интервалов нет");
+        } else {
+            System.out.printf("Пересечение интервала: (%.2f, %.2f)%n", range3.getFrom(), range3.getTo());
+        }
 
+        Range[] range4 = range.getAssociation(range2);
+        System.out.printf("Объединение двух интервалов, новый интервал: %s%n", Range.toString(range4));
 
+        Range[] range5 = range.getSubtraction(range2);
+        if (range5 == null) {
+            System.out.println("Разности двух интервалов нет.");
+        } else {
+            System.out.printf("Разности двух интервалов, новый интервал: %s%n", Range.toString(range5));
+        }
 
+        System.out.println();
         System.out.println("Введите x: ");
         double x = in.nextDouble();
 
         boolean inside = range.isInside(x);
-        System.out.printf("Данное число %.2f принадлежит диапазону: %b ", x, inside);
+        System.out.printf("Данное число %.2f принадлежит первому диапазону: %b ", x, inside);
     }
 }
--- Range/src/ru/academits/space/range/Range.java
+++ Range/src/ru/academits/space/range/Range.java
@@ -1,8 +1,10 @@
 package ru.academits.space.range;
 
+import java.util.Arrays;
 
 public class Range {
 
+    private static final double EPSILON = 1.0e-10;
     private double from;
     private double to;
 
@@ -11,20 +13,17 @@ public class Range {
         this.to = to;
     }
 
+    public static String toString(Range[] rangeArray) {
+        return Arrays.toString(rangeArray);
+
+    }
+
     public double getFrom() {
         return from;
     }
 
-    public void setFrom(double from) {
-        this.from = from;
-    }
-
     public double getTo() {
         return to;
-    }
-
-    public void setTo(double to) {
-        this.to = to;
     }
 
     public double calcLength() {
@@ -39,8 +38,37 @@ public class Range {
         }
     }
 
+    public Range[] getAssociation(Range range2) {
+
+        if (Math.min(to, range2.to) < Math.max(from, range2.from)) {
+            return new Range[]{new Range(Math.min(from, range2.from), Math.min(to, range2.to)),
+                    new Range(Math.max(from, range2.from), Math.max(to, range2.to))};
+        } else {
+            return new Range[]{new Range(Math.min(from, range2.from), Math.max(to, range2.to))};
+        }
+    }
+
+    public Range[] getSubtraction(Range range2) {
+        if ((to < range2.from)) {                                                                  // 3
+            return new Range[]{new Range(from, to)};
+        } else if ((from < range2.from) && (range2.to < to)) {                                     // 4
+            return new Range[]{new Range(from, range2.from), new Range(range2.to, to)};
+        } else if (from < range2.from) {                                                           // 1
+            return new Range[]{new Range(from, range2.from)};
+        } else if ((from - range2.from <= EPSILON) && (range2.to < to)) {                          // 2
+            return new Range[]{new Range(range2.to, to)};
+        } else {
+            return null;
+        }
+    }
 
     public boolean isInside(double x) {
         return x >= from && x <= to;
     }
 }
+
+
+
+
+
+
