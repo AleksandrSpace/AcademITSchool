c41eac95d408a2b7a72e98f8f8dbd2f4d9df1683
Александр <aleksandrpustota@gmail.com>
2017-02-20 20:34:28 +0700

    задача CFT

--- CFT/src/ru/academits/space/maincft/MainCFT.java
+++ CFT/src/ru/academits/space/maincft/MainCFT.java
@@ -1,6 +1,7 @@
 package ru.academits.space.maincft;
 
 import ru.academits.space.insertionsort.InsertionSortDecrease;
+import ru.academits.space.insertionsort.InsertionSortIncrease;
 import ru.academits.space.insertionsort.InsertionSortStringDecrease;
 import ru.academits.space.insertionsort.InsertionSortStringIncrease;
 
@@ -19,76 +20,67 @@ public class MainCFT {
             return;
         }
 
-        try (Scanner scan = new Scanner(new FileInputStream(args[0]));
-             PrintWriter writer = new PrintWriter(args[1])) {
+        if (args[0].equals("inputCFTString.txt") || args[0].equals("inputCFTInt.txt")) {
 
-            if (args[2].equals("-i")) {
-
-                if (args[3].equals("-a")) {
-
-                    ArrayList<Integer> line = new ArrayList<>(20);
-                    while (scan.hasNextInt()) {
-                        line.add(scan.nextInt());
-                    }
-
-                    for (Object e : InsertionSortStringIncrease.sorting(line)) {
-                        writer.println(e);
-                    }
-
-                } else if (args[3].equals("-d")) {
+            try (Scanner scan = new Scanner(new FileInputStream(args[0]));
+                 PrintWriter writer = new PrintWriter(args[1])) {
+                if (args[0].equals("inputCFTInt.txt") && args[2].equals("-i")) {
 
                     ArrayList<Integer> line = new ArrayList<>(20);
                     while (scan.hasNextInt()) {
                         line.add(scan.nextInt());
                     }
 
-                    for (Object e : InsertionSortDecrease.sorting(line)) {
-                        writer.println(e);
+                    if (args[3].equals("-a")) {
+
+                        for (Object e : InsertionSortIncrease.sorting(line)) {
+                            writer.println(e);
+                        }
+
+                    } else if (args[3].equals("-d")) {
+
+                        for (Object e : InsertionSortDecrease.sorting(line)) {
+                            writer.println(e);
+                        }
+
+                    } else {
+                        System.out.println(" Не коректный аргумент. ");
                     }
 
-                } else {
-                    System.out.println(" Не коректный аргумент. ");
-
-                }
-
-            } else if (args[2].equals("-s")) {
-
-                if (args[3].equals("-a")) {
+                } else if (args[0].equals("inputCFTString.txt") && args[2].equals("-s")) {
 
                     ArrayList<String> line = new ArrayList<>(20);
                     while (scan.hasNextLine()) {
                         line.add(scan.nextLine());
                     }
 
-                    for (Object e : InsertionSortStringIncrease.sorting(line)) {
-                        writer.println(e);
-                    }
+                    if (args[3].equals("-a")) {
 
-                } else if (args[3].equals("-d")) {
+                        for (Object e : InsertionSortStringIncrease.sorting(line)) {
+                            writer.println(e);
+                        }
 
-                    ArrayList<String> line = new ArrayList<>(20);
-                    while (scan.hasNextLine()) {
-                        line.add(scan.nextLine());
-                    }
+                    } else if (args[3].equals("-d")) {
 
-                    for (Object e : InsertionSortStringDecrease.sorting(line)) {
-                        writer.println(e);
+                        for (Object e : InsertionSortStringDecrease.sorting(line)) {
+                            writer.println(e);
+                        }
+
+                    } else {
+                        System.out.println(" Не коректный аргумент. ");
                     }
 
                 } else {
-                    System.out.println(" Не коректный аргумент. ");
-
+                    System.out.println(" Тип не соответствует заданной папке.");
                 }
 
-            } else {
-                System.out.println(" Не коректный тип. ");
-
+            } catch (FileNotFoundException e) {
+                System.out.println(" Не обнаружено указанного файла");
             }
 
-        } catch (FileNotFoundException e) {
-            System.out.println(" Не обнаружено указанного файла");
+        } else {
+            System.out.println(" Не неправильно указан файл");
         }
     }
 }
-
 
