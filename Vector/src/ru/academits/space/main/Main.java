fd3fd4335ea9047c2072cebfd6805ea942ac0249
Александр <aleksandrpustota@gmail.com>
2017-03-06 19:45:48 +0700

    Задача  Vector

--- Vector/src/ru/academits/space/main/Main.java
+++ Vector/src/ru/academits/space/main/Main.java
@@ -5,98 +5,104 @@ import ru.academits.space.vector.Vector;
 public class Main {
     public static void main(String[] args) {
 
-        double[] a = {3, 6, 9.0001, 0, 0, 0};
-        Vector vector = new Vector(a);
-        System.out.printf("Вектор1: %s%n", vector.toString());
-        System.out.printf("Размерность вектора1; %d%n", vector.getSize());
+        try {
 
-        double[] b = {7, 4, 1};
-        Vector vector2 = new Vector(b);
-        System.out.printf("Вектор2: %s%n", vector2.toString());
-        System.out.printf("Размерность вектора2 %d%n", vector2.getSize());
+            double[] a = {3, 6, 9.0001, 0, 0, 0};
+            Vector vector = new Vector(a);
+            System.out.printf("Вектор1: %s%n", vector.toString());
+            System.out.printf("Размерность вектора1; %d%n", vector.getSize());
 
-        int n = 10;
-        Vector vector3 = new Vector(n);
-        System.out.printf("Вектор3: %s%n", vector3.toString());
-        System.out.printf("Размерность вектора3: %d%n", vector3.getSize());
+            double[] b = {7, 4, 1};
+            Vector vector2 = new Vector(b);
+            System.out.printf("Вектор2: %s%n", vector2.toString());
+            System.out.printf("Размерность вектора2 %d%n", vector2.getSize());
 
-        Vector vector4 = new Vector(vector);
-        System.out.printf("Вектор4: %s%n", vector4.toString());
-        System.out.printf("Размерность вектора4: %d%n", vector4.getSize());
+            int n = 10;
+            Vector vector3 = new Vector(n);
+            System.out.printf("Вектор3: %s%n", vector3.toString());
+            System.out.printf("Размерность вектора3: %d%n", vector3.getSize());
 
-        Vector vector5 = new Vector(b, n);
-        System.out.printf("Вектор5: %s%n", vector5.toString());
-        System.out.printf("Размерность вектора5: %d%n", vector5.getSize());
+            Vector vector4 = new Vector(vector);
+            System.out.printf("Вектор4: %s%n", vector4.toString());
+            System.out.printf("Размерность вектора4: %d%n", vector4.getSize());
 
-        Vector vector6 = vector.getAddition(vector2);
-        System.out.printf("Полученный сложением вектор6 %s%n", vector6);
+            Vector vector5 = new Vector(b, n);
+            System.out.printf("Вектор5: %s%n", vector5.toString());
+            System.out.printf("Размерность вектора5: %d%n", vector5.getSize());
 
-        double[] a2 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a2);
-        double[] b2 = {7, 4, 1};
-        vector2 = new Vector(b2);
+            Vector vector6 = vector.getAddition(vector2);
+            System.out.printf("Полученный сложением вектор6 %s%n", vector6);
 
-        Vector vector7 = vector.getSubtraction(vector2);
-        System.out.printf("Полученный вычитанием вектор7: %s%n", vector7);
+            double[] a2 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a2);
+            double[] b2 = {7, 4, 1};
+            vector2 = new Vector(b2);
 
-        double[] a3 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a3);
-        int scalar = 3;
+            Vector vector7 = vector.getSubtraction(vector2);
+            System.out.printf("Полученный вычитанием вектор7: %s%n", vector7);
 
-        Vector vector8 = vector.getMultiplicationScalar(scalar);
-        System.out.printf("Полученный умножением на скаляр вектор8: %s%n", vector8);
+            double[] a3 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a3);
+            int scalar = 3;
 
-        double[] a4 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a4);
+            Vector vector8 = vector.getMultiplicationScalar(scalar);
+            System.out.printf("Полученный умножением на скаляр вектор8: %s%n", vector8);
 
-        Vector vector9 = vector.getUnfolding();
-        System.out.printf("Полученный разворот вектор9: %s%n", vector9);
+            double[] a4 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a4);
 
-        double[] a5= {3, 3, 3, 3, 0, 0};
-        vector = new Vector(a5);
+            scalar = -1;
+            Vector vector9 = vector.getMultiplicationScalar(scalar);
+            System.out.printf("Полученный разворот вектор9: %s%n", vector9);
 
-        System.out.printf("Полученный  длина вектор10: %f%n", vector.getLength());
+            double[] a5 = {3, 3, 3, 3, 0, 0};
+            vector = new Vector(a5);
 
-        double[] a6 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a6);
+            System.out.printf("Полученный  длина вектор10: %f%n", vector.getLength());
 
-        int index = 2;
-        System.out.printf("Элемент под индексом %d = %f%n", index, vector.getIndex(index));
+            double[] a6 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a6);
 
-        int index2 = 2;
-        double x = 5.6;
-        vector3.setIndex(index2, x);
-        System.out.printf("Полученный вектор10 с новым значением: %s%n", vector3);
+            int index = 2;
+            System.out.printf("Элемент под индексом %d = %f%n", index, vector.getComponentsIndex(index));
 
-        double[] a7 = {3, 6, 9.0000, 0, 0, 0};
-        vector = new Vector(a7);
-        double[] b3 = {3, 6, 9.0001, 0, 0, 0};
-        vector2 = new Vector(b3);
+            int index2 = 2;
+            double x = 5.6;
+            vector3.setComponentsIndex(index2, x);
+            System.out.printf("Полученный вектор10 с новым значением: %s%n", vector3);
 
-        System.out.printf("Проверка на equals: %s%n", vector.equals(vector2));
-        System.out.printf("hashCode = %d%n", vector.hashCode());
+            double[] a7 = {3, 6, 9.0000, 0, 0, 0};
+            vector = new Vector(a7);
+            double[] b3 = {3, 6, 9.00000, 0, 0, 0};
+            vector2 = new Vector(b3);
 
-        double[] a8 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a8);
-        double[] b4 = {7, 4, 1};
-        vector2 = new Vector(b4);
+            System.out.printf("Проверка на equals: %s%n", vector.equals(vector2));
+            System.out.printf("hashCode = %d%n", vector.hashCode());
 
-        Vector vector11 = Vector.getAddition2(vector, vector2);
-        System.out.printf("Полученный сложением вектор11: %s%n", vector11);
+            double[] a8 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a8);
+            double[] b4 = {7, 4, 1};
+            vector2 = new Vector(b4);
 
-        double[] a9 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a9);
-        double[] b5 = {7, 4, 1};
-        vector2 = new Vector(b5);
+            Vector vector11 = Vector.getAddition(vector, vector2);
+            System.out.printf("Полученный сложением вектор11: %s%n", vector11);
 
-        Vector vector12 = Vector.getSubtraction2(vector, vector2);
-        System.out.printf("Полученный вычитанием вектор12: %s%n", vector12);
+            double[] a9 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a9);
+            double[] b5 = {7, 4, 1};
+            vector2 = new Vector(b5);
 
-        double[] a10 = {3, 6, 9.0001, 0, 0, 0};
-        vector = new Vector(a10);
+            Vector vector12 = Vector.getSubtraction(vector, vector2);
+            System.out.printf("Полученный вычитанием вектор12: %s%n", vector12);
 
-        Vector vector13 = Vector.getMultiplicationScalar2(vector, scalar);
-        System.out.printf("Полученный умножением на скаляр вектор13: %s%n", vector13);
+            double[] a10 = {3, 6, 9.0001, 0, 0, 0};
+            vector = new Vector(a10);
+
+            Vector vector13 = Vector.getMultiplicationScalar(vector, scalar);
+            System.out.printf("Полученный умножением на скаляр вектор13: %s%n", vector13);
+        } catch (IllegalArgumentException e) {
+            System.out.println( e.getMessage());
+        }
     }
 }
 
--- Vector/src/ru/academits/space/vector/Vector.java
+++ Vector/src/ru/academits/space/vector/Vector.java
@@ -5,18 +5,26 @@ import java.util.Arrays;
 
 public class Vector {
 
-    final double epsilon = 0.0001;
+    //   final double epsilon = 0.0001;
     private double[] vector;
 
-    public Vector(int n) {
-        vector = new double[n];
+    public Vector(int n){
+        if (n <= 0){
+            throw new IllegalArgumentException ("Длина массива меньше нуля.");
+        }
+    vector = new double[n];
     }
 
     public Vector(Vector vector) {
+        double[] old = vector.vector;
+        System.arraycopy(old, 0, vector.vector, 0, old.length);
         this.vector = vector.vector;
     }
 
     public Vector(double[] vector) {
+
+        System.arraycopy(vector, 0, vector, 0, vector.length);
+
         this.vector = vector;
     }
 
@@ -24,18 +32,18 @@ public class Vector {
         this.vector = Arrays.copyOf(vector, n);
     }
 
-    private static void Expanding2(Vector vector1, Vector vector2) {
-        int a = Math.max(vector1.vector.length, vector2.vector.length);
+    private static void expanding(Vector vector1, Vector vector2) {
+        int maxLength = Math.max(vector1.vector.length, vector2.vector.length);
         if (vector1.vector.length < vector2.vector.length) {
             double[] old = vector1.vector;
-            vector1.vector = new double[a];
+            vector1.vector = new double[maxLength];
             System.arraycopy(old, 0, vector1.vector, 0, old.length);
         }
     }
 
-    public static Vector getAddition2(Vector vector1, Vector vector2) {
+    public static Vector getAddition(Vector vector1, Vector vector2) {
 
-        Vector.Expanding2(vector1, vector2);
+        Vector.expanding(vector1, vector2);
 
         for (int i = 0; i < vector2.vector.length; ++i) {
             vector1.vector[i] += vector2.vector[i];
@@ -43,9 +51,9 @@ public class Vector {
         return vector1;
     }
 
-    public static Vector getSubtraction2(Vector vector1, Vector vector2) {
+    public static Vector getSubtraction(Vector vector1, Vector vector2) {
 
-        Expanding2(vector1, vector2);
+        expanding(vector1, vector2);
 
         for (int i = 0; i < vector2.vector.length; ++i) {
             vector1.vector[i] -= vector2.vector[i];
@@ -53,7 +61,7 @@ public class Vector {
         return vector1;
     }
 
-    public static Vector getMultiplicationScalar2(Vector vector1, int x) {
+    public static Vector getMultiplicationScalar(Vector vector1, int x) {
 
         for (int i = 0; i < vector1.vector.length; ++i) {
             vector1.vector[i] *= x;
@@ -70,22 +78,18 @@ public class Vector {
         return s1.replace("]", "}");
     }
 
-    public String toString(double[] vector) {
-        return Arrays.toString(vector).replace("[", "{").replace("]", "}");
-    }
-
-    private void Expanding(Vector vector) {
-        int a = Math.max(this.vector.length, vector.vector.length);
+    private void expanding(Vector vector) {
+        int maxLength = Math.max(this.vector.length, vector.vector.length);
         if (this.vector.length < vector.vector.length) {
             double[] old = this.vector;
-            this.vector = new double[a];
+            this.vector = new double[maxLength];
             System.arraycopy(old, 0, this.vector, 0, old.length);
         }
     }
 
     public Vector getAddition(Vector vector) {
 
-        Expanding(vector);
+        expanding(vector);
 
         for (int i = 0; i < vector.vector.length; ++i) {
             this.vector[i] += vector.vector[i];
@@ -95,7 +99,7 @@ public class Vector {
 
     public Vector getSubtraction(Vector vector) {
 
-        Expanding(vector);
+        expanding(vector);
 
         for (int i = 0; i < vector.vector.length; ++i) {
             this.vector[i] -= vector.vector[i];
@@ -110,7 +114,7 @@ public class Vector {
         }
         return this;
     }
-
+/*
     public Vector getUnfolding() {
 
         for (int i = 0; i < this.vector.length; ++i) {
@@ -118,35 +122,39 @@ public class Vector {
         }
         return this;
     }
+    */
 
     public double getLength() {
 
         double sum = 0;
-        for (int i = 0; i < this.vector.length; ++i) {
+        Vector vector1 = new Vector(this.vector);
 
-            this.vector[i] = (Math.pow(this.vector[i], 2));
+        for (int i = 0; i < vector1.vector.length; ++i) {
+
+            this.vector[i] = (Math.pow(vector1.vector[i], 2));
             sum += this.vector[i];
         }
         return Math.abs(Math.sqrt(sum));
     }
 
-    public double getIndex(int i) {
+    public double getComponentsIndex(int i) {
         return this.vector[i];
     }
 
-    public double setIndex(int i, double x) {
+    public double setComponentsIndex(int i, double x) {
         return this.vector[i] = x;
     }
 
-    private boolean isEpsilon(Vector vector) {
+    /*
+        private boolean isEpsilon(Vector vector) {
 
-        for (int i = 1; i < this.vector.length - 1; ++i)
-            if (Math.abs(this.vector[i] - vector.vector[i]) >= epsilon) {
-                return false;
-            }
-        return true;
-    }
-
+            for (int i = 1; i < this.vector.length - 1; ++i)
+                if (Math.abs(this.vector[i] - vector.vector[i]) >= epsilon) {
+                    return false;
+                }
+            return true;
+        }
+    */
     public boolean equals(Object o) {
         if (o == this) {
             return true;
@@ -155,16 +163,18 @@ public class Vector {
             return false;
         }
         Vector vector = (Vector) o;
-        return (this.vector.length == vector.vector.length) && isEpsilon(vector);
+        return (this.vector.length == vector.vector.length) && Arrays.equals(this.vector, vector.vector);    //isEpsilon(vector);
     }
 
     public int hashCode() {
-        final int prime = 11;
-        int hash = 1;
-        for (int i = 0; i < this.vector.length; ++i) {
-            hash = prime * hash + (int) this.vector[i];
-        }
-        return hash;
+//
+        //final int prime = 11;
+        //int hash = 1;
+        //for (int i = 0; i < this.vector.length; ++i) {
+        //hash = prime * hash + (int) this.vector[i];
+        //}
+        //
+        return Arrays.hashCode(vector);
     }
 }
 
